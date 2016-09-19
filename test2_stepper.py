9a866528b895dc951b4ace19d53ae01bc6fc9a3d
Karl Preisner <karlpreisner@gwu.edu>
2016-09-19 13:13:18 -0400

    adjusted sleep time between moves

--- test2_stepper.py
+++ test2_stepper.py
@@ -10,11 +10,11 @@ GPIOsetup()
 
 for i in range (0,3):
 	stepClockwise_withAccel(tableSteps/8, speed2)
-	time.sleep(3.0)
+	time.sleep(2.0)
 
 for i in range (0,3):
 	stepCounterClockwise_withAccel(tableSteps/8, speed2)
-	time.sleep(3.0)
+	time.sleep(2.0)
 
 # Test with no acceleration
 #step_clockwise(tableSteps/2, speed4)
