52cda0126c390d9d792e5464cf7e3715f51edb12
Karl Preisner <karlpreisner@gwu.edu>
2016-11-09 16:04:14 -0500

    ran demo, changes variables around while running. need to commit.

--- test2_stepper.py
+++ test2_stepper.py
@@ -8,12 +8,12 @@ import time
 #setup pins 
 GPIOsetup()
 
-for i in range (0,3):
-	stepClockwise_withAccel(tableSteps/8, speed2)
+for i in range (0,1):
+	stepClockwise_withAccel(tableSteps/2, speed6)
 	time.sleep(2.0)
 
-for i in range (0,3):
-	stepCounterClockwise_withAccel(tableSteps/8, speed2)
+for i in range (0,1):
+	stepCounterClockwise_withAccel(tableSteps/2, speed6)
 	time.sleep(2.0)
 
 # Test with no acceleration
